e2c8702217c9a723099ae98aeeda8de8827a3a50
kris aziabor <krisaaziabor@gmail.com>
2024-06-26 15:29:04 -0400

    created queries.js

---
+++ queries.js
@@ -0,0 +1,2 @@
+// THIS FILE IS TO STORE ALL QUERIES and minimize the code in "linear.js".
+// THIS WILL ALSO RENDER QUERY_HISTORY redundant and once this is finalized, the file will be removed from the repo.
